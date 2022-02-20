ca06453fc225bc898df44948d98b4d34a2c2b7bb
yugykim <yugyeongk92@gmail.com>
2022-02-20 05:48:30 -0700

    create new login

--- hackathon2022/src/views/login.vue
+++ hackathon2022/src/views/login.vue
@@ -1,39 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <div class="login">
-    <div class="container">
-      <div class="main-writing">
-        <h1>Log in to MOOD SPOT</h1>
-        <form action="action_page.php" method="post">
-          <div class="login-container">
-            <label for="email" id="email"><b>Email</b></label>
-            <input
-              type="text"
-              placeholder="Enter Email"
-              name="uname"
-              id="email_input"
-              required
-            />
-
-            <label for="pw" id="pw"><b>Password</b></label>
-            <input
-              type="password"
-              placeholder="Enter Password"
-              name="pw"
-              id="pw_input"
-              required
-            />
-          </div>
-        </form>
-      </div>
-      <div class="button" type="submit">
-        <router-link to="/first-question">
-          <NextBtn msg="Login" />
-        </router-link>
-      </div>
-    </div>
-  </div>
-=======
 <div class="login">
 
         <div class="container">
@@ -47,7 +12,7 @@
             </div>
         </div>
 </div>
->>>>>>> bba217b1ffd6fc73b71cc99595f4c2d388dab07b
+
 </template>
 
 <script>
@@ -55,12 +20,6 @@
 import NextBtn from "@/components/NextBtn.vue";
 
 export default {
-<<<<<<< HEAD
-  name: "login",
-  components: {
-    NextBtn,
-  },
-=======
     name: "login",
     components: {
         NextBtn,
@@ -80,59 +39,11 @@ export default {
             localStorage.username = newName
         }
     }
-
->>>>>>> bba217b1ffd6fc73b71cc99595f4c2d388dab07b
 };
 </script>
 
 <style scoped>
 .login {
-<<<<<<< HEAD
-  box-sizing: border-box;
-  height: 100vh;
-  background: url("../img/bg-1.gif");
-  background-size: 100%;
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  background-size: cover;
-}
-
-.main-writing {
-  text-align: center;
-}
-
-.container {
-  padding-top: 3vw;
-}
-
-.login-container {
-  width: 20vw;
-  height: 20vw;
-  padding-top: 5vw;
-  margin: auto;
-  display: flex;
-  flex-direction: column;
-  text-align: left;
-  margin: 4rem;
-}
-
-#email_input,
-#pw_input {
-  margin-bottom: 2vw;
-}
-
-.home-image {
-  flex: 4;
-  margin: 0.2vw;
-  padding: 15vw;
-}
-
-.button {
-  flex: 2;
-  margin: 2rem;
-  text-align: center;
-=======
     box-sizing: border-box;
     height: 100vh;
     background: url("../img/bg-1.gif");
